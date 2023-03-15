ac954a61ee7c0aa295be4e0a7740e6e0d27e9548
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-03-15 16:15:14 -0600

    mongodb atlas connection functional + package

---
+++ pages/api/mongodb.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { MongoClient } from "mongodb"
+
+export default async function mongoHandler(req: NextApiRequest, res: NextApiResponse){
+    const client = await MongoClient.connect("mongodb+srv://konstantin:58OdYd6WUR22hyuQ@cluster0.9jtuxx0.mongodb.net/liveScene?retryWrites=true&w=majority");
+    const db = client.db();
+    const collection = db.collection("testing")
+    await collection.insertOne({ hello: "yes "})
+    client.close();
+}
