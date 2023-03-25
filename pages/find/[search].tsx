bfe6f6358cf42acae43f313078c765cd81b99eb1
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-03-24 20:20:57 -0600

    misc console log fixes

--- pages/find/[search].tsx
+++ pages/find/[search].tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react"
 import { AppDispatch, useAppSelector, store } from "@/redux/store"
 import { useDispatch } from "react-redux"
-import { ChangeEvent } from "react"
 import { rehydrate, fetchConcerts } from "@/redux/slices/allConcerts/allConcertsSlice"    
 import { GetServerSideProps, InferGetServerSidePropsType } from "next"
 import { useRouter } from "next/router"
@@ -12,14 +11,12 @@ export const getServerSideProps:GetServerSideProps = async(context) => {
     let concerts = {};
 
     if(city){
-        console.log(await store.dispatch(fetchConcerts(`${(city as string).split(' ')[0]}+${(city as string).split(' ')[1]}`)));
+        await store.dispatch(fetchConcerts(`${(city as string).split(' ')[0]}+${(city as string).split(' ')[1]}`));
         concerts = store.getState().allConcerts.concerts;
 
         if(concerts){
             return {
-                props: {
-                    concerts: concerts
-                }
+                props: { concerts }
             }
         }
     }
@@ -28,22 +25,18 @@ export const getServerSideProps:GetServerSideProps = async(context) => {
 export default function FindConcerts({ concerts }: InferGetServerSidePropsType<typeof getServerSideProps>){
     const dispatch = useDispatch<AppDispatch>();
     const router = useRouter();
-    const [value, setValue] = useState<number>(0)
     const { query = {} } = router || {};
     const { search: city = undefined } = query || {};
-
-    console.log(concerts)
 
     useEffect(() => {
         if(concerts.length < 1 && router.isReady && city){
             dispatch(rehydrate(concerts));
         }
-    }, [router.isReady, value])
+    }, [router.isReady])
 
     return(
         <div className="find">
             <p>hello</p>
-            <input type="number" value={value} onChange={(e: any) => setValue(e.target.value)} />
         </div>
     )
 }
