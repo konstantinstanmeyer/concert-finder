2e7a8ff1afaec88b8fecd3163645cc1c5a0b964d
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-03-29 22:09:19 -0600

    getArtists page algorithms setup

---
+++ util/getArtists.ts
@@ -0,0 +1,11 @@
+// let results = [];
+// things._embedded.events.map(event => {
+//   event._embedded.attractions.filter(attraction => attraction.name === "The Flaming Lips").forEach(concert => {
+//   results = [...results, {
+//     eventName: event._embedded.venues[0].name,
+//     artist: concert.name,
+//     location: event._embedded.venues[0].city.name + event._embedded.venues[0].state.stateCode,
+//   }];
+// })})
+
+// console.log(results);
